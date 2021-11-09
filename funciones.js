b4bab58320895b6a0839fa9c61467ef9f53e4fd0
DolphinMaster25 <92401292+DolphinMaster25@users.noreply.github.com>
2021-11-09 10:33:51 -0600

    Add files via upload

---
+++ funciones.js
@@ -0,0 +1,35 @@
+function Encender(){
+    document.getElementById("img").src="img/Bombillo2.png";
+
+}
+
+function Apagar(){
+    document.getElementById("img").src="img/Bombillo.png";
+
+}
+
+function CambiaVerde(){
+    document.getElementById("fondo").style.backgroundColor="LightGreen";
+    document.getElementById("conte").style.backgroundColor="LightGreen";
+
+}
+
+function CambiaBlanco(){
+    document.getElementById("conte").style.backgroundColor="White";
+    document.getElementById("fondo").style.backgroundColor="White";
+    
+}
+
+function agregar(){
+    var lista = document.getElementById("lista");
+    var dato = document.getElementById("Dato").value;
+    var li = document.createElement("li");
+    li.textContent = dato;
+    lista.appendChild(li);
+}
+
+function eliminar(){
+    var lista = document.getElementById("lista");
+    lista.removeChild(lista.lastElementChild);
+}
+
